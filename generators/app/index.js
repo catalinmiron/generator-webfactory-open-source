ab8304eaf1d10e17ccdd15923a840b541d8bb3e7
Matthias Molitor <mmo@webfactory.de>
2015-06-12 18:24:58 +0200

    changed formatting

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,43 +4,45 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the gnarly ' + chalk.red('WebfactoryOpenSource') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the gnarly ' + chalk.red('WebfactoryOpenSource') + ' generator!'
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        var prompts = [{
+            type: 'confirm',
+            name: 'someOption',
+            message: 'Would you like to enable this option?',
+            default: true
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_LICENSE'),
-        this.destinationPath('LICENSE')
-      );
-      this.fs.copy(
-        this.templatePath('_README.md'),
-        this.destinationPath('README.md')
-      );
+    writing: {
+        app: function () {
+            this.fs.copyTpl(
+                this.templatePath('_LICENSE'),
+                this.destinationPath('LICENSE'),
+                {}
+            );
+            this.fs.copyTpl(
+                this.templatePath('_README.md'),
+                this.destinationPath('README.md'),
+                {}
+            );
+        }
+    },
+
+    install: function () {
+        this.installDependencies();
     }
-  },
-
-  install: function () {
-    this.installDependencies();
-  }
 });
