404d6dee36766b4c4ca60adb209a80d9d7805f60
Matthias Molitor <mmo@webfactory.de>
2015-06-19 10:58:24 +0200

    changed greeting text

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,7 +9,7 @@ module.exports = yeoman.generators.Base.extend({
 
         // Have Yeoman greet the user.
         this.log(yosay(
-            'Welcome to the gnarly ' + chalk.red('WebfactoryOpenSource') + ' generator!'
+            'Welcome to the ' + chalk.red('webfactory Open Source') + ' project setup!'
         ));
 
         var prompts = [];
