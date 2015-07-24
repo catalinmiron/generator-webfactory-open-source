1455531252518b56bd0bca591aa7bb6da2bffa81
Matthias Molitor <mmo@webfactory.de>
2015-07-24 15:17:35 +0200

    re-organized tests; added test stubs for composer.json content (#5)

--- test/test-app.js
+++ test/test-app.js
@@ -4,61 +4,82 @@ var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
-describe('webfactory-open-source:app', function () {
-    before(function (done) {
-        helpers.run(path.join(__dirname, '../generators/app'))
-            .withOptions({
-                skipInstall: true,
-                repositoryUrl: "git@github.com:webfactory/generator-webfactory-open-source.git"
-            })
-            .on('end', done);
+describe('open source generator', function () {
+    describe('called with dashed repository name', function () {
+        before(function (done) {
+            helpers.run(path.join(__dirname, '../generators/app'))
+                .withOptions({
+                    skipInstall: true,
+                    repositoryUrl: "git@github.com:webfactory/generator-webfactory-open-source.git"
+                })
+                .on('end', done);
+        });
+
+        it('creates a license file', function () {
+            assert.file([
+                'LICENSE'
+            ]);
+        });
+
+        it('adds current year to license', function () {
+            assert.fileContent('LICENSE', new RegExp(new Date().getFullYear().toString()));
+        });
+
+        it('creates a README file', function () {
+            assert.file([
+                'README.md'
+            ]);
+        });
+
+        it('adds current year to README', function () {
+            assert.fileContent('README.md', new RegExp(new Date().getFullYear().toString()));
+        });
+
+        it('adds Travis URL to README', function () {
+            assert.fileContent('README.md', /https:\/\/travis-ci.org/);
+        });
+
+        it('adds Travis configuration', function () {
+            assert.file([
+                '.travis.yml'
+            ]);
+        });
+
+        it('adds Composer configuration', function () {
+            assert.file([
+                'composer.json'
+            ]);
+        });
+
+        it('adds .gitignore file', function () {
+            assert.file([
+                '.gitignore'
+            ]);
+        });
+
+        it('adds editor config', function () {
+            assert.file([
+                '.editorconfig'
+            ]);
+        });
     });
 
-    it('creates a license file', function () {
-        assert.file([
-            'LICENSE'
-        ]);
-    });
+    describe('called with camel cased repository name', function () {
+        before(function (done) {
+            helpers.run(path.join(__dirname, '../generators/app'))
+                .withOptions({
+                    skipInstall: true,
+                    repositoryUrl: "git@github.com:WebFactory/GeneratorWebfactoryOpenSource.git"
+                })
+                .on('end', done);
+        });
 
-    it('adds current year to license', function () {
-        assert.fileContent('LICENSE', new RegExp(new Date().getFullYear().toString()));
-    });
+        it('adds dashed vendor name in composer.json', function () {
 
-    it('creates a README file', function () {
-        assert.file([
-            'README.md'
-        ]);
-    });
+        });
 
-    it('adds current year to README', function () {
-        assert.fileContent('README.md', new RegExp(new Date().getFullYear().toString()));
-    });
+        it('adds dashed repository name in composer.json', function () {
 
-    it('adds Travis URL to README', function () {
-        assert.fileContent('README.md', /https:\/\/travis-ci.org/);
-    });
-
-    it('adds Travis configuration', function () {
-        assert.file([
-            '.travis.yml'
-        ]);
-    });
-
-    it('adds Composer configuration', function () {
-        assert.file([
-            'composer.json'
-        ]);
-    });
-
-    it('adds .gitignore file', function () {
-        assert.file([
-            '.gitignore'
-        ]);
-    });
-
-    it('adds editor config', function () {
-        assert.file([
-            '.editorconfig'
-        ]);
+        });
     });
 });
